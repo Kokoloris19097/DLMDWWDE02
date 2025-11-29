493232321dde7379957bbca29133d5d41624ff66
D0292168 <patrick.luber@dm.de>
2025-11-29 16:18:10 +0100

    Entfernen von Clickhouse aufgrund von Ressourcen- und Thread-Pool-Limit.

--- kafka-clickhouse-connector/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/kafka/bin/connect-distributed.sh /kafka/config/connect-distributed.properties &
-sleep 20  # Warte, bis Connect bereit ist
-curl -X POST -H "Content-Type: application/json" --data @/kafka/connectors/config.json http://localhost:8083/connectors
-wait
